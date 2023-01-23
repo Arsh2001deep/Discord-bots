c992905d1f18c13fd2a6aa700012579942a4a6f6
Arsh2001deep <arshdeep.singh.fiem.cse20@teamfuture.in>
2023-01-23 08:40:37 +0530

    hi hello

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const { Client, IntentsBitField, messageLink } = require('discord.js');
+
+
+
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+
+});
+
+
+client.login(process.env.TOKEN)
+
+client.on('ready', () => {
+    console.log('bot is online')
+
+})
+client.on("messageCreate", (message) => {
+    if (message.content == "hi") { message.reply("hello") }
+})
